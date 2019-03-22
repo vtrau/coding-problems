3a46103dcc958f76058cb319ec469df880eb1d78
Vincent Trau <trauvincent@pop-os.localdomain>
2019-03-21 22:58:09 -0700

    Problem 1 brute force

---
+++ twoSum [Easy]/twoSum.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+public class twoSum {
+
+  public static int[] twoSumBruteForce(int[] nums, int target) {
+    for (int i = 0; i < nums.length; i++) {
+      for (int j = i + 1; j < nums.length; j++) {
+        if (nums[j] == target - nums[i]) {
+          return new int[] { i, j };
+        }
+      }
+    }
+    throw new IllegalArgumentException("No two sum solution");
+  }
+  public static void main(String[] args) {
+      // Prints "Hello, World" to the terminal window.
+      System.out.println("Hello, World");
+      int[] array = {1, 4, 45, 6, 10, -8};
+      int n = 16;
+      System.out.println(Arrays.toString(twoSum(array, n)));
+  }
+}
